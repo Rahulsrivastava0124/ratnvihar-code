4295e2ff317bb3edcf9cffa266608283b5595db0
RatnVihar <prakriti.jewels02@gmail.com>
2024-05-30 13:52:01 +0530

    Initial commit

---
+++ ratnvihar_frontend/src/actions/axios.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import {getAuthData} from 'src/helpers/helper';
+
+const access_token = getAuthData('access_token');
+const instance = axios.create({
+    baseURL: 'http://localhost:9083' + '/api',
+    headers: {
+      Authorization: access_token ? 'Bearer ' + access_token : ''
+    }
+});
+
+export default instance;
