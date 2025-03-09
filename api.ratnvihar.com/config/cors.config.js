31244585078aa573186a33d4df65af0c4e51b494
Rahul srivastava <87309230+Rahulsrivastava0124@users.noreply.github.com>
2025-03-10 04:48:38 +0530

    Update cors.config.js

--- api.ratnvihar.com/config/cors.config.js
+++ api.ratnvihar.com/config/cors.config.js
@@ -21,7 +21,8 @@ module.exports = {
             "https://admin.ratnvihar.com",
             "https://team.ratnvihar.com",
             "https://ratnvihar.com",
-	        "https://www.ratnvihar.com"
+	        "https://www.ratnvihar.com",
+		https://dr.drsumananandjha.com/
         ]
     }
 };
