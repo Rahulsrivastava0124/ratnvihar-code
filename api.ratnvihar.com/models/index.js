58f036c8a69743476929e6b7f698aeafe0306bc2
Rahul srivastava <87309230+Rahulsrivastava0124@users.noreply.github.com>
2025-03-11 00:57:23 +0530

    add  the password

--- api.ratnvihar.com/models/index.js
+++ api.ratnvihar.com/models/index.js
@@ -15,7 +15,7 @@ const config = {
   use_env_variable: false, // Set to 'DATABASE_URL' if using an environment variable
   database: 'ratnvihar',
   username: 'mysql',
-  password: 'dhcnahbirpa1kmzo', // this is passowrd
+  password: 'ratnvihar', // this is passowrd
   host: '147.93.110.206',
   dialect: 'mysql',
   port: 3306,
